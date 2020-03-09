b7eb98d7d8ac110ef18dada10ba40e82a846684c
AidanDumont <61623140+AidanDumont@users.noreply.github.com>
2020-03-09 22:16:28 +0100

    Add files via upload

---
+++ Dumont_Aidan_FV2.py
@@ -0,0 +1,48 @@
+﻿# fonction de base 10 en base 16
+n=int(input('Quelle nombre en décimal voulez vous son équivalent en hexadécimal'))
+
+def hexaDigits (n: int)->str:
+
+    if n<10 :
+        return str(n)
+    elif n== 10 :
+        return 'A'
+    elif n== 11 :
+        return 'B'
+    elif n== 12 :
+        return 'C'
+    elif n== 13 :
+        return 'D'
+    elif n== 14 :
+        return 'E'
+    elif n== 15 :
+        return 'F'
+    else:
+        print('?')
+print(hexaDigits(n))
+
+#
+
+def fillBits (n: int)->int:
+    n = str(n)
+    L = len(n)
+    while L%4 != 0:
+        n = '0' + n
+        L = len(n)
+    return n
+print (fillBits(n))
+
+#Fonction décimal a binaire
+
+n = int(input('Entrez un nombre entier en décimal'))
+def dec2Bin (n:int)->int:
+    R = ''
+    while n != 0:
+        R = R + str(n%2)
+        n = n//2
+    return R
+print(dec2Bin(n))
+
+#
+
+
---
+++ Morpiondelamor.py
@@ -0,0 +1,61 @@
+﻿lun = [1, 0, 1]
+lde = [0, 0, 0]
+ltroa = [1, 1, 1]
+gry = [lun, lde, ltroa]
+#gry = [[0, 0, 0],[0, 0, 0],[0, 0, 0]]
+
+def affichage(tableau:list):
+    n = len(tableau)
+    for i in range(n):
+        print(tableau[i])
+def demande (numJou : int)->list:
+    if numJou == 1:
+        print("o tor du jeur 1")
+    else:
+        print("o tor du jeur 2")
+    rigne, coronnu = input("rigne, coronnu").split(",")
+    return [int(rigne), int(coronnu)]
+
+def verf(tableau : list)->int:
+    sommes=[0,0,0,0,0,0,0,0]
+    #grille[0]=lun grille[1]=lde grille[2]=ltroa if sum(grille[0]):
+    #lignes
+    sommes[0] = tableau[0][0] + tableau[0][1] + tableau [0][2]
+    sommes[1] = tableau[1][0] + tableau[1][1] + tableau [1][2]
+    sommes[2] = tableau[2][0] + tableau[2][1] + tableau [2][2]
+    #colonnes
+    sommes[3] = tableau[0][0] + tableau[1][0] + tableau [2][0]
+    sommes[4] = tableau[0][1] + tableau[1][1] + tableau [2][1]
+    sommes[5] = tableau[0][2] + tableau[1][2] + tableau [2][2]
+    #diagonales
+    sommes[6] = tableau[0][0] + tableau[1][1] + tableau [2][2]
+    sommes[7] = tableau[0][2] + tableau[1][1] + tableau [2][0]
+    for element in sommes :
+        if element ==3: return 1
+        elif element == -3: return -1
+    return 0
+
+
+
+
+
+
+def play():
+    gry =[[0, 0, 0],
+         [0, 0, 0],
+         [0, 0, 0]]
+
+    joueur = 1
+    gagner = verf(gry)
+    while (gagner != "X" or gagner != "O"):
+        rigne, coronnu = demande(joueur)
+        gry [rigne][coronnu] = joueur
+        affichage(gry)
+        gagner = verf(gry)
+        if gagner == "X": print ("Jeur1 a gane")
+        elif gagner == "O": print("Jeur2 a gane")
+        joueur = -joueur
+
+
+winner = play()
+print(winner)
